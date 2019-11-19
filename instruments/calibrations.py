e3b6208a8f409f1732ba3c05bd7d405c4c3825ec
Paul Anzel <anzel.paul@heb.com>
2019-11-19 13:14:45 -0600

    Forse sensors code

---
+++ instruments/calibrations.py
@@ -0,0 +1,33 @@
+# Forces in Newtons, voltages in Volts.
+# To test, run command
+# $ python force_sensors_code.py SENSORID
+force_sensors = {
+    'C300510': {
+        'forces':   [10.0, 20.0, 30.0, 40.0, 50.0, 60.0],
+        'voltages': [0.65, 1.31, 2.01, 2.80, 3.79, 4.92]
+    },
+    'C300511': {
+        'forces':   [10.0, 20.0, 30.0, 40.0, 50.0, 60.0],
+        'voltages': [0.64, 1.37, 2.03, 2.84, 3.75, 4.97]
+    },
+    'C401423': {
+        'forces':   [1.01, 2.00, 3.05, 3.98, 5.03, 6.04],
+        'voltages': [0.53, 1.07, 1.62, 2.14, 2.78, 3.57]
+    },
+    'C401424': {
+        'forces':   [1.01, 2.00, 3.05, 3.98, 5.03, 6.04],
+        'voltages': [0.65, 1.32, 2.00, 2.74, 3.39, 4.25]
+    },
+}
+
+# Response is scaled at lowest frequency. See base response for mV/Pa.
+# To test, run command
+# $ python microphone_code.py MICROPHONEID
+microphones = {
+    'NGY0543': {
+        'frequency': [100,  200,  300,  400,  500,  600,  700,  800,  900, 1000],
+        'response':  [  1, 1.05, 1.07, 1.25, 5.04, 2.21, 0.93, 0.63, 0.27, 0.11],
+        'phase': [0, 0.01, 0.05, 0.17, 0.54, 1.45, 2.56, 3.00, 3.05, 3.12, 3.13],
+        'base_response': 0.276
+    }
+}
---
+++ instruments/force_sensors_code.py
@@ -0,0 +1,56 @@
+"""Some basic routines for working with the force sensors.
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+from scipy.interpolate import CubicSpline
+import sys
+from calibrations import force_sensors as FORCE_SENSORS
+
+
+def plot_force_sensor(sensor_id):
+    try:
+        data = FORCE_SENSORS[sensor_id]
+    except:
+        raise ValueError(
+            "Unknown sensor ID %s provided, available sensors in calibrations.py"
+            % sensor_id)
+    forces = np.array(data['forces'])
+    voltages = np.array(data['voltages'])
+    slope, _, _, _, _ = linregress(voltages, forces)
+
+    spline_x = np.linspace(0, max(voltages))
+    spline_y = get_force_for_voltage(spline_x, sensor_id)
+
+    plt.figure(figsize=(5, 5), dpi=150)
+    plt.scatter(forces, 1000*voltages, color='k', marker='o', label="Measured values")
+    plt.plot(spline_x, 1000*spline_y, color='k', linewidth=2, linestyle="--", label="Interpolation")
+    plt.title("Sensor {device}, {factor:.2e} N/V".format(device=sensor_id, factor=slope), fontsize=16)
+    plt.legend(fontsize=16)
+    plt.xlabel("Force [N]", fontsize=16)
+    plt.ylabel("Voltage [mV]", fontsize=16)
+
+
+def get_force_for_voltage(voltages, sensor_id):
+    """Gets the force for a given voltage with a particular device. Uses cubic interpolation.
+    
+    Inputs:
+        voltage (float or float array): Voltage response.
+        sensor_id (str): Device serial number.
+    Outputs:
+        force (float or float array): Estimated force.
+    """
+    try:
+        data = FORCE_SENSORS[sensor_id]
+    except:
+        raise ValueError(
+            "Unknown sensor ID %s provided, available sensors in calibrations.py"
+            % sensor_id)
+    spline = CubicSpline(data['voltages'], data['forces'], bc_type='not-a-knot')
+    return spline(voltages)
+
+
+if __name__ == '__main__':
+    sensor = sys.argv[1]
+    plot_force_sensor(sensor)
