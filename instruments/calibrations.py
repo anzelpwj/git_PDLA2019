2d4f89fb41103aebf3b578a7376ea430036463bb
Paul Anzel <anzelpwj@users.noreply.github.com>
2019-12-03 11:15:32 -0800

    Add dummy calibration

--- instruments/calibrations.py
+++ instruments/calibrations.py
@@ -1,5 +1,9 @@
 # Forces in Newtons, voltages in Volts.
 force_sensors = {
+    'DUMMY': {
+        'forces':   [1, 2, 3, 4, 5],
+        'voltages': [1, 2, 3, 4, 5]
+    },
     'C300510': {
         'forces':   [10.0, 20.0, 30.0, 40.0, 50.0, 60.0],
         'voltages': [0.65, 1.31, 2.01, 2.80, 3.79, 4.92]
